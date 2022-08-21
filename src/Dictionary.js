d0f12ed035efc2f1c213344288600fe86dbbbe4f
Asma Mohamed Lamin <as.mohamedlamine@gmail.com>
2022-08-21 19:40:46 +0200

    Aadded a form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,24 @@
-import react from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 export default function Dictionary() {
-  return <div className="Dictionary">hello from Asma</div>;
+  let [keyWord, setKeyWord] = useState("");
+  function searchForm(event) {
+    event.preventDefault();
+    alert(` hello from ${keyWord}`);
+  }
+  function handleKwyWordChange(event) {
+    setKeyWord(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={searchForm}>
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKwyWordChange}
+        ></input>
+        <input type="submit" value="submit"></input>
+      </form>
+    </div>
+  );
 }
